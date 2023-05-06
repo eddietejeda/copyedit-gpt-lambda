26ee9c18fe35afd2da5a0e6123b011f1bfd79e29
Eddie A Tejeda <eddie.tejeda@gmail.com>
2023-05-06 15:04:30 -0700

    use JSON as output format and add support to run locally and in lambda

--- lambda_function.py
+++ lambda_function.py
@@ -3,36 +3,46 @@ import json
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 
-if os.environ.get("ENV") == "development":
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
+def copyedit(event):
 
-def copyedit():
     llm = OpenAI(model_name="text-davinci-003")
 
+    user_input = event["user_input"]
+
     template = """
-    Please lightly copy edit the following text: {user_input}
+    Lightly copy edit the following text: {user_input}
 
-    After you print the output text, add a line break, and then print a bulleted list of what you changed.
+    The output will be in a JSON string format, where the revised text will go in a 'revision' field, and the list of changes will go in the 'changes' field using markdown bullets syntax.
     """
-    user_input = input("Enter your text: \n\n")
-
 
     prompt = PromptTemplate(
         input_variables=["user_input"],
         template=template,
     )
+
     final_prompt = prompt.format(user_input=user_input)
 
     return (llm(final_prompt))
 
-def lambda_handler(event, context):
+def lambda_handler(event):
+    
     return {
         'statusCode': 200,
-        'body': json.dumps( copyedit() )
+        'body': json.loads(copyedit(event))
     }
 
 
+
 if os.environ.get("ENV") == "development":
-    print(f"\n\nOutput text:\n {copyedit()}")
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+    user_input = input("Enter your text: \n\n")
+    event = {
+        "user_input": user_input
+    }
+
+    response = lambda_handler(event)
+
+    print(f"\n\nRevised text:\n\n{response['body']['revision']}\n\nChanges:\n{response['body']['changes']}")
