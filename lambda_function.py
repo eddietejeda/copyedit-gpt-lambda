c9486cc9e76cbd51a5fd84295ffc1492a6b64716
Eddie A Tejeda <eddie.tejeda@gmail.com>
2023-05-07 16:49:05 -0700

    permissive access for now. allow cross origin access.

--- lambda_function.py
+++ lambda_function.py
@@ -19,6 +19,11 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'isBase64Encoded': True,
+        'headers': {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         'body': base64.b64encode(response) 
     }
     
